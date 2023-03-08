e05470d6290f812d54ea26195e1da7bfd2a75840
LaraGutz_Mac <laragutz@gmail.com>
2023-03-07 18:21:29 -0600

    editando archivos

--- s00_06mar23/src/App.js
+++ s00_06mar23/src/App.js
@@ -1,10 +1,40 @@
+// Importaciones de recursos externos
+
+// Importaciones de recursos internos
+import Card from './components/Card';
+
+// Importar assets, estilos, etc.
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+
 
 function App() {
+
+  const tittle = "Sesiòn 00 Mi primer React";
+  const imgPet = "https://picsum.photos/id/237/200/300";
+  const pharagraph = "Lorem ipsum dolor sit amet co nsectetur adipisicing elit. Quisquam, quod.";
+  
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{tittle}</h1>
+        <img src={imgPet} className="Puppy-Pet" alt="puppy pet" />
+        <p>{pharagraph}</p>
+      </header>
+
+      {
+      // Componente con etiqueta autocierre
+    }
+      <Card/>
+
+      {
+      // Componente con etiqueta de apertura y cierre
+    }
+      <Card></Card>
+
+      {
+        /*
+              <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +48,8 @@ function App() {
           Learn React
         </a>
       </header>
+        */
+      }
     </div>
   );
 }
---
+++ s00_06mar23/src/components/Card.js
@@ -0,0 +1,9 @@
+function Card(){
+    return(
+        <div className="card">
+            This is a card
+        </div>
+    )
+}
+
+export default Card;
